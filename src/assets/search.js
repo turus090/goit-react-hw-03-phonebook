44331f165fa5c8c91f0ffac3cdd8b3070ee0cde7
turus090 <102972952+turus090@users.noreply.github.com>
2023-07-24 06:56:02 +0000

    Deploying to gh-pages from @ turus090/goit-react-hw-03-phonebook@082b1e41cac69e9e374a9b9a7fe2a60e72c1c770 🚀

--- src/assets/search.js
+++
@@ -1,7 +0,0 @@
-const search = (name, searchInput) => {
-    let nameLowerCase = name.toLowerCase()
-    let searchLowerCase = searchInput.toLowerCase()
-    return nameLowerCase.startsWith(searchLowerCase)
-   }
-   
-   export default search
